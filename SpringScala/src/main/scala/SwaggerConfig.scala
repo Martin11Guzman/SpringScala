85bc51290bcc96525d62f9baf2fcea52159d512c
martin11guzman <mguzman@cspire.com>
2017-09-20 12:59:53 -0500

    Security REST APIs with Spring Security

--- SpringScala/src/main/scala/SwaggerConfig.scala
+++ SpringScala/src/main/scala/SwaggerConfig.scala
@@ -1,9 +1,4 @@
-import javax.tools.DocumentationTool.DocumentationTask
-
-import jdk.nashorn.internal.ir.RuntimeNode.Request
 import org.springframework.context.annotation.Bean
-import org.springframework.web.context.support.RequestHandledEvent
-import springfox.documentation.RequestHandler
 import springfox.documentation.builders.{PathSelectors, RequestHandlerSelectors}
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
---
+++ SpringScala/src/main/scala/WebSecurityConfig.scala
@@ -0,0 +1,32 @@
+import javax.activation.DataSource
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.{EnableWebSecurity, WebSecurityConfigurerAdapter}
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl
+
+
+//  This Spring security configuration enables basic HTTP authentication. HTTP authentication is required to access the APIs after this change.
+//  The users inserted into the database using import.sql root/root can be used to authenticate.
+
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class WebSecurityConfig(@Autowired val dataSource: DataSource) extends WebSecurityConfigurerAdapter {
+  override def configure(http: HttpSecurity) = {
+    http.authorizeRequests.antMatchers("/console", "/console/**", "/console/", "/swagger-ui.html", "/**/*.css", "/**/*.js", "/**/*.png", "/configuration/**", "/swagger-resources", "/v2/**").permitAll
+    http.authorizeRequests.anyRequest.authenticated
+    http.csrf.disable
+    http.headers.frameOptions.disable
+    http.httpBasic
+  }
+
+  @Bean override def userDetailsService: UserDetailsService = {
+    val manager = new JdbcDaoImpl
+    manager.setDataSource(dataSource)
+    manager
+  }
+}
