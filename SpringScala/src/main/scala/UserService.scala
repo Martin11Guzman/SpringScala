3b9ad356abcfe7be1d1176700db1d8e77fe2f503
martin11guzman <mguzman@cspire.com>
2017-09-20 13:22:20 -0500

    Added Method-level validations: userRoles

--- SpringScala/src/main/scala/UserService.scala
+++ SpringScala/src/main/scala/UserService.scala
@@ -1,14 +1,20 @@
+import com.google.common.util.concurrent.Service
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
+import org.springframework.security.access.prepost.{PostAuthorize, PreAuthorize}
 
 @Service
 class UserService(@Autowired private val userRepository: UserRepository) {
+  @PreAuthorize("hasRole('admin')")
   def listUsers(): Iterable[Users] = {
-      userRepository.findAll
+    userRepository.findAll
   }
+
+  @PreAuthorize("hasRole('user')")
+  @PostAuthorize("returnObject.username==principal.username || hasRole('admin')")
   def getUser(id: Long): Users = {
     userRepository.findOne(id)
   }
+  @PreAuthorize("hasRole('admin')")
   def createUser(users: Users): Long = {
     userRepository.save(users)
     users.id
