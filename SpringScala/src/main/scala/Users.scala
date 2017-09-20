9dfb17219be7e5d7249b875dd3de16a647e53ffb
martin11guzman <mguzman@cspire.com>
2017-09-20 10:29:57 -0500

    Added Entity class

---
+++ SpringScala/src/main/scala/Users.scala
@@ -0,0 +1,25 @@
+import javax.persistence.{Column, Entity, GeneratedValue}
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
+import com.fasterxml.jackson.databind.BeanProperty
+
+@Entity
+class Users extends Serializable {
+  @Id
+  @GeneratedValue
+  @BeanProperty
+  var id: Long = _
+
+  @BeanProperty
+  @Column(name = "username")
+  var username: String = _
+
+  @BeanProperty
+  @Column(name = "password")
+  var password: String = _
+
+  @BeanProperty
+  @Column(name = "enabled")
+  var enabled: Boolean = _
+  
+}
